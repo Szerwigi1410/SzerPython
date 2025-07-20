b8aed6698ddb0a06e022150a067a647b282faa88
Szerwigi1410 <szerwigi14@gmail.com>
2025-07-20 20:15:41 +0200

    Create SzerPython.py

---
+++ SzerPython.py
@@ -0,0 +1,23 @@
+#funckje
+disp = print
+nie = False
+tak = True
+hw = "Hello World!"
+ws = "Witaj Świecie"
+
+
+
+
+disp("Welcome to SherPython!")
+disp("Szerwigi's very own programming language!")
+disp("Instead of print use disp")
+disp("Remember: nie means False and tak means True")
+
+#cośtamy
+pytanie = input("Wanna start coding? tak/nie: ")
+
+if pytanie == True or pytanie == "tak":
+    disp("So lets start!")
+else:
+    disp("Syabu YSN")
+    exit()
